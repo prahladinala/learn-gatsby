d40edfb1b2cd26859fe85bdf60cacce30a507827
Prahlad Inala <prahlad2018@gmail.com>
2021-09-18 12:01:32 +0530

    Styles using global styling and module styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
-          <h2>Design</h2>
+          <h2 >Design</h2>
           <h3>Develop & Deploy</h3>
-          <p>UX Designer & web Developer based in India.</p>
+          <p>UX Designer & Web Developer based in India.</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
       </section >
-    </Layout>
+    </Layout >
 
   )
 }
