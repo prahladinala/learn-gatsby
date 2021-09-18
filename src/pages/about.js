c6668ac370df5d9ae17b373d0029abfc9e82f83f
Prahlad Inala <prahlad2018@gmail.com>
2021-09-18 07:00:07 +0530

    Navbar Created

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 export default function About() {
     return (
--- src/pages/projects/text.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Test() {
-    return (
-        <div>
-            <h2>Test</h2>
-        </div>
-    )
-}
