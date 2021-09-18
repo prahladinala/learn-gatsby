0f8a75102bf7c499ecd3621bf0449767fe92106c
Prahlad Inala <prahlad2018@gmail.com>
2021-09-18 06:51:49 +0530

    Pages and Routes

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ab suscipit magni repudiandae dolorum? Asperiores blanditiis maxime quibusdam, fugit saepe excepturi doloremque magnam est laboriosam repellendus doloribus dicta iure consequatur.
+                Incidunt accusantium nihil eligendi, odit officia, temporibus repudiandae quidem ipsum exercitationem perspiciatis laboriosam nisi. Vero aut reiciendis, tempora omnis aliquam culpa incidunt voluptatibus corporis repellendus cupiditate nobis, dolorem fuga ab.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ab suscipit magni repudiandae dolorum? Asperiores blanditiis maxime quibusdam, fugit saepe excepturi doloremque magnam est laboriosam repellendus doloribus dicta iure consequatur.
+                Incidunt accusantium nihil eligendi, odit officia, temporibus repudiandae quidem ipsum exercitationem perspiciatis laboriosam nisi. Vero aut reiciendis, tempora omnis aliquam culpa incidunt voluptatibus corporis repellendus cupiditate nobis, dolorem fuga ab.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ab suscipit magni repudiandae dolorum? Asperiores blanditiis maxime quibusdam, fugit saepe excepturi doloremque magnam est laboriosam repellendus doloribus dicta iure consequatur.
+                Incidunt accusantium nihil eligendi, odit officia, temporibus repudiandae quidem ipsum exercitationem perspiciatis laboriosam nisi. Vero aut reiciendis, tempora omnis aliquam culpa incidunt voluptatibus corporis repellendus cupiditate nobis, dolorem fuga ab.</p>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>UX Designer & web Developer based in India.</p>
+      </div>
+    </section>
+  )
 }
