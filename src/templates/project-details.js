3df0525051862a9db3c16abccf499e5893356a04
Prahlad Inala <prahlad2018@gmail.com>
2021-09-19 18:52:23 +0530

    Template Components

---
+++ src/templates/project-details.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import * as styles from '../styles/project-details.module.css'
+import Img from 'gatsby-image'
+
+export default function ProjectDetails() {
+    return (
+        <Layout>
+            <div className={styles.details}>
+                <h2>title</h2>
+                <h3>stack</h3>
+                <div className={styles.featured}>
+                    <Img fluid={ } />
+                </div>
+                <div className={styles.html} dangerouslySetInnerHTML={ } />
+            </div>
+        </Layout>
+    )
+}
