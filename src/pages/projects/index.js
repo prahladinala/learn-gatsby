965acde1973fc0b01a862f19263673ccee46c70d
Prahlad Inala <prahlad2018@gmail.com>
2021-09-19 18:43:36 +0530

    Featured Image

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
+import Img from 'gatsby-image'
 
 export default function Projects({ data }) {
     console.log(data)
@@ -16,6 +17,7 @@ export default function Projects({ data }) {
                     {projects.map(project => (
                         <Link to={'/projects/' + project.frontmatter.slug} key={project.id}>
                             <div>
+                                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                                 <h3>{project.frontmatter.title}</h3>
                                 <p>{project.frontmatter.stack}</p>
                             </div>
@@ -30,21 +32,29 @@ export default function Projects({ data }) {
 
 // EXPORT PAGE QUERY
 export const query = graphql`
-    query ProjectsPage {
-        projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
-            nodes {
-              frontmatter {
-                title
-                stack
-                slug
+query ProjectsPage {
+    projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          title
+          stack
+          slug
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
               }
-              id
             }
+          }
         }
-        contact: site {
-            siteMetadata {
-                contact
-            }
-        }
+        id
+      }
     }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+  
 `
